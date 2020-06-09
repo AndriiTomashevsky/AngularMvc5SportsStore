eff02373335f36e0565ee8ae3e9d0f5394b8d2ac
AndriiTomashevsky <an-t@ukr.net>
2020-06-09 16:34:21 +0300

    added repository to clientapp

---
+++ ClientApp/src/app/models/repository.ts
@@ -0,0 +1,11 @@
+import { Product } from "./product.model";
+
+export class Repository
+{
+  constructor()
+  {
+    this.product = JSON.parse(document.getElementById("data").textContent);
+  }
+
+  product: Product;
+}
