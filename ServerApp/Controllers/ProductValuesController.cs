148304507a8d6ec6d3d7e6aac4aa680ca201f396
AndriiTomashevsky <an-t@ukr.net>
2020-06-09 16:30:58 +0300

    added web api to serverapp

---
+++ ServerApp/Controllers/ProductValuesController.cs
@@ -0,0 +1,29 @@
+﻿using ServerApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ServerApp.Controllers
+{
+    [RoutePrefix("api/products")]
+    public class ProductValuesController : ApiController
+    {
+        private DataContext context;
+
+        public ProductValuesController(DataContext ctx)
+
+        {
+            context = ctx;
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public Product GetProduct(long id)
+        {
+            return context.Products.Find(id);
+        }
+    }
+}
