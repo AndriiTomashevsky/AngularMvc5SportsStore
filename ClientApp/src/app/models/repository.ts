a0d14e8de81f0196eefa071c99210afa24a22112
AndriiTomashevsky <an-t@ukr.net>
2020-06-09 19:37:51 +0300

    temp

--- ClientApp/src/app/models/repository.ts
+++ ClientApp/src/app/models/repository.ts
@@ -1,11 +1,17 @@
 import { Product } from "./product.model";
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
 
 export class Repository
 {
   constructor()
   {
-    this.product = JSON.parse(document.getElementById("data").textContent);
+    this.getProduct(1);
   }
 
-  product: Product;
+  getProduct(id: number)
+  {
+    this.http.get<Product>("/api/products/" + id)
+      .subscribe(p => this.product = p);
+  }
 }
