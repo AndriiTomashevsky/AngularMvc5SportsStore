adac604c52ce53b9152ff2388efefec327dd3c38
AndriiTomashevsky <an-t@ukr.net>
2020-06-18 20:08:49 +0300

    added routing

--- ServerApp/App_Start/RouteConfig.cs
+++ ServerApp/App_Start/RouteConfig.cs
@@ -14,6 +14,12 @@ namespace ServerApp
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "angular_fallback",
+                url: "{target:regex(table|detail)}/{*catchall}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
